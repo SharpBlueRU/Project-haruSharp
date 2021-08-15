b7d109b45d0eb8b45bcbe1df7f8ee879aa499597
SharpBluexd <83186507+SharpBluexd@users.noreply.github.com>
2021-08-15 13:14:14 -0300

    Adicionando arquivos front-end

---
+++ entrar.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/bulma.css">
+    <link rel="stylesheet" href="styles/style.css">
+    <title>Entrar</title>
+</head>
+<body>
+    <section class="section">
+        <div class="container">
+          <div class="columns is-centered">
+            <div class="column is-half">
+              <h1 class="title has-text-centered">Entrar - haruSharp <span>cloud</span></h1>
+  
+              <form class="box" action="" method="post">
+  
+                <div class="field">
+                    <label class="label">Código de Acesso</label>
+                      <div class="control is-loading">
+                        <input class="input" type="email" placeholder="Digite seu código de acesso">
+                      </div>
+                  </div>
+            
+                <div class="field">
+                  <label class="label">Email</label>
+                    <div class="control">
+                      <input class="input" type="email" placeholder="Digite seu email">
+                    </div>
+                </div>
+  
+                  <div class="field">
+                    <label class="label">Senha</label>
+                       <div class="control">
+                        <input class="input" type="password" placeholder="Dgite sua senha">
+                       </div>
+                   </div>
+                  
+                   <div class="field">
+                    <div class="control">
+                      <a href="registro.html">Faça seu registro</a>
+                    </div>
+                 </div>
+
+                  <div class="field">
+                    <div class="control">
+                      <button class="button is-link is-fullwidth">Entrar</button>
+                    </div>
+                 </div>
+
+                
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+</body>
+</html>
