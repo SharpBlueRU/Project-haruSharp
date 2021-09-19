67cdd1f3241d6d53e1ab821405578fdf5c110e31
SharpBluexd <83186507+SharpBluexd@users.noreply.github.com>
2021-09-18 21:31:32 -0300

    ...

--- index.php
+++ index.php
@@ -1,120 +1,18 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="styles/bulma.css">
-  <link rel="stylesheet" href="styles/style.css">
-  <title>haruSharp cloud</title>
-</head>
-
-<body>
-  <nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-item">
-      <a href="navbar-item">
-        <h1 class="title is-4">haru<span>Sharp</span></h1>
-      </a>
-      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-  
-    <div class="navbar-menu">
-      <div class="navbar-start">
-        <a class="navbar-item" href="quemsomos.php">
-          Quem somos?
-        </a>
-        
-         <a class="navbar-item" href="planos.php">
-          Planos
-        </a>
-          
-        <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link" href="">
-          Mais
-        </a>
-        <div class="navbar-dropdown is-boxed">
-          <a class="navbar-item" href="parceiros.php">
-          Parceiros
-          </a>
-          <a class="navbar-item" href="politicas-e-termos.php">
-            Politícas e Termos
-          </a>
-          <a class="navbar-item" href="suporte.php">
-            Suporte
-          </a>
-        </div>
-      </div>
-      
-      
-      </div>
-   </div>
-  
-      <div class="navbar-end">
-        <div class="navbar-item">
-          <div class="buttons">
-            <a class="button is-black" href="registro.php">
-              <strong>Registro</strong>
-            </a>
-            <a class="button is-light" href="entrar.php">
-              Login
-            </a>
-
-          </div>
-        </div>
-      </div>
-    </div>
-  </nav>
-
-  <section class="section">
-    <div class="container">
-      <h1 class="title has-text-centered is-2">haruSharp <span>cloud</span></h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem officiis animi deleniti facere voluptatem
-        consectetur sit provident maxime tenetur eaque. Harum deserunt nesciunt incidunt maxime saepe, nam atque maiores
-        dolores.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem ducimus omnis ab dicta ad, reprehenderit id
-        sint neque sequi porro est dolore minima unde quae? Sunt minima reprehenderit ea fugit! <br> <br> 
-        <strong><span>Confira</span></strong> <br>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum optio praesentium sint maxime quasi quaerat esse, iste architecto laboriosam consectetur magni nesciunt! Eligendi quisquam distinctio rem voluptatem maiores, cupiditate amet!
-      </p>
-    </div>
-  </section>
-
-  <section class="section">
-    <div class="content has-text-centered">
-      <div class="columns ">
-        <div class="column">
-          <p class="title">Plano Free</p>
-          <div class="columns ">
-            <div class="column">
-              <div class="box">
-                <span class="icon is-large">
-                  <i class="fas fa-check fa-2x"></i>
-                </span>
-                <p class="subtitle is-5">Com esse plano você tera 100 gigas de armazenamento <br> livre para uso, apenas fazendo o seu cadastro <br> em nossa plataforma.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="column">
-          <p class="title">Plano Premium</p>
-          <div class="columns">
-            <div class="column">
-              <div class="box">
-                <span class="icon is-large">
-                  <i class="fas fa-rocket fa-2x"></i>
-                </span>
-                <p class="subtitle">Nesse plano, você irá ter acesso a 2 teras de <br> armazenamento disponível para uso, pagando apenas uma <br> taxa por mês</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-</body>
-</html>
+<?php
+   $mobile = FALSE;
+   $user_agents = array("iPhone","iPad","Android","webOS","BlackBerry","iPod","Symbian","IsGeneric");
+ 
+   foreach($user_agents as $user_agent){
+     if (strpos($_SERVER['HTTP_USER_AGENT'], $user_agent) !== FALSE) {
+        $mobile = TRUE;
+	$modelo	= $user_agent;
+	break;
+     }
+   }
+ 
+   if ($mobile){
+      header('Location:pagina-indisponivel.php');
+   }else{
+      header('Location:inicio.php');
+   }
+?>
